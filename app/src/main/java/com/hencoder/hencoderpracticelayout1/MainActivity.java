d81634e921228c820dfc9a4967abf36a63767ff4
mny <mny9@outlook.com>
2019-01-31 10:54:03 +0800

    联系布局过程的含义

--- app/src/main/java/com/hencoder/hencoderpracticelayout1/MainActivity.java
+++ app/src/main/java/com/hencoder/hencoderpracticelayout1/MainActivity.java
@@ -1,5 +1,6 @@
 package com.hencoder.hencoderpracticelayout1;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.StringRes;
@@ -48,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         tabLayout = findViewById(R.id.tabLayout);
+        tabLayout.setTabTextColors(Color.GREEN, Color.BLACK);
         tabLayout.setupWithViewPager(pager);
     }
 
@@ -57,9 +59,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private class PageModel {
-        @LayoutRes int sampleLayoutRes;
-        @StringRes int titleRes;
-        @LayoutRes int practiceLayoutRes;
+        @LayoutRes
+        int sampleLayoutRes;
+        @StringRes
+        int titleRes;
+        @LayoutRes
+        int practiceLayoutRes;
 
         PageModel(@LayoutRes int sampleLayoutRes, @StringRes int titleRes, @LayoutRes int practiceLayoutRes) {
             this.sampleLayoutRes = sampleLayoutRes;
